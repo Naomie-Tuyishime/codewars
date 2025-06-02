99cb5d2ec200e79455e2891e02e98c0cee1759f7
Naomie2003 <tuyishimenaome58@gmail.com>
2025-06-02 15:16:24 +0200

    commit3

--- kata-2.js
+++ kata-2.js
@@ -1,7 +1,15 @@
+/* Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true  when no 'x' and 'o' is present should return true
+ XO("zzoo") => false*/
+
 function XO(str) {
   str= str.toLowerCase();
-
-    //code here
   let a= str.split('o').length-1
   let b=str.split('x').length-1
   if (a===b){
