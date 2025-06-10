e199b4d1a83ea9194aee024a0f6464190afa8abf
Naomie2003 <tuyishimenaome58@gmail.com>
2025-06-10 10:44:38 +0200

    commit7

---
+++ kata7.js
@@ -0,0 +1,14 @@
+ 
+/* Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"*/
+ 
+ 
+ function reverseWords(str) {
+  // Go for it
+ return  str.split(" ").map(words =>words.split("").reverse().join("")).join(" ");
+  
+}
+reverseWords( "sihT si na !elpmaxe");
